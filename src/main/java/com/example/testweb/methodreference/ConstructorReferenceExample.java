5396820f8534b9b8292d7de87cb20f747394df6f
Wilson <hereisleedo@gmail.com>
2023-12-04 20:41:30 +0800

    加入方法引用範例

---
+++ src/main/java/com/example/testweb/methodreference/ConstructorReferenceExample.java
@@ -0,0 +1,36 @@
+package com.example.testweb.methodreference;
+
+import java.util.function.Supplier;
+
+public class ConstructorReferenceExample {
+    //建構子引用
+    public static void main(String[] args) {
+        constructorReference();
+        System.out.println("========上面使用建構子引用===========");
+        lambda();
+        System.out.println("========上面使用lambda==============");
+        normal();
+
+
+    }
+
+    public static void constructorReference(){
+        Supplier<StringBuilder> supplier = StringBuilder::new;
+        StringBuilder stringBuilder = supplier.get();
+        stringBuilder.append("Hello, Constructor Reference!");
+        System.out.println(stringBuilder.toString());
+    }
+    public static void lambda(){
+        Supplier<StringBuilder> supplier = () -> new StringBuilder();
+        StringBuilder stringBuilder = supplier.get();
+        stringBuilder.append("Hello, Constructor Lambda!");
+        System.out.println(stringBuilder.toString());
+    }
+
+    public static void normal(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Hello, It is normal!");
+        System.out.println(stringBuilder.toString());
+    }
+
+}
---
+++ src/main/java/com/example/testweb/methodreference/ThisInstanceMethodReference.java
@@ -0,0 +1,29 @@
+package com.example.testweb.methodreference;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ThisInstanceMethodReference {
+//特定類的實例方法引用（使用 this）
+    public static void main(String[] args) {
+        ThisInstanceMethodReference example = new ThisInstanceMethodReference();
+        example.runExamplesByInstanceMethodReference();
+        System.out.println("=====上面使用限定實例this=====");
+        example.runExamplesByLambda();
+        System.out.println("=====上面使用lambda==========");
+    }
+
+    public void runExamplesByInstanceMethodReference() {
+        List<String> list = Arrays.asList("Java", "Kotlin", "Scala");
+        list.forEach(this::printLanguages);
+    }
+
+    public void runExamplesByLambda(){
+        List<String> list = Arrays.asList("Java", "Kotlin", "Scala");
+        list.forEach(s -> printLanguages(s));
+    }
+
+    private void printLanguages(String lang) {
+        System.out.println(lang);
+    }
+}
